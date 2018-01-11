38a8a97fefcb8b76278e2174cb4963a72f9580fe
Daniel Nop <danielnop@daniels-air.lan>
2018-01-11 18:20:47 -0500

    Products table populated, prompts are storing value

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,29 @@
+var mysql = require("mysql");
 var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bamazon"
+});
+	
+connection.connect(function(err) {
+  if (err) throw err;
+  //Select all products and return the result object:
+  connection.query("SELECT * FROM products", function (err, result, fields) {
+    if (err) throw err;
+
+    console.log(result)
+
+   
+  });
+});
+
 inquirer.prompt([
 	    {
       type: "input",
-      message: "What is the product ID associated with the product you would like to purchase?",
+      message: "What is the item ID associated with the product you would like to purchase?",
       name: "product ID"
 	  },
 	  {
@@ -11,5 +32,11 @@ inquirer.prompt([
 	  name: "qty"
 	  }
 
-	  ]);
+
+
+	  ]). then(function(selections){
+	  	
+	  	console.log(selections);
+
+	  });
     
